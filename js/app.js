f8d340f6cf7f148912117cbf87ba9e580eaf402f
ThitZinAung <87572410+ThitZinAung@users.noreply.github.com>
2024-03-11 20:20:39 +0630

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,19 +25,6 @@ window.addEventListener('beforeunload', function() {
     }, 3000); // Adjust the delay as needed
 });
 
-
-window.addEventListener('scroll', function () {
-    var scroll = window.scrollY;
-    console.log(scroll);
-    if (scroll >= 50) {
-        document.querySelector(".sticky").classList.add("stickyadd");
-    } else {
-        document.querySelector(".sticky").classList.remove("stickyadd");
-    }
-
-});
-
-
 var typed = new Typed("#element", {
     strings: ["Thit Zin Aung", "Web Developer", "Frontend Developer", "Backend Developer"],
     smartBackspace: true,
@@ -47,6 +34,56 @@ var typed = new Typed("#element", {
     loopCount: Infinity,
     startDelay: 1000
 });
+
+// Sticky and Active Link
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+        const targetNavLink = document.querySelector('.navbar-nav a[href="#' + sectionId + '"]');
+
+        if (targetNavLink) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                targetNavLink.classList.add('active-link');
+            } else {
+                targetNavLink.classList.remove('active-link');
+            }
+        }
+    });
+
+    const homeSection = document.getElementById('home');
+    const homeLink = document.querySelector('.navbar-nav a[href="#home"]');
+
+    if (scrollY >= homeSection.offsetTop - 50 && scrollY < homeSection.offsetTop + homeSection.offsetHeight - 50) {
+        homeLink.classList.add('active-link');
+    } else {
+        homeLink.classList.remove('active-link');
+    }
+}
+
+function handleStickyHeader() {
+    var scroll = window.scrollY;
+
+    if (scroll >= 50) {
+        document.querySelector(".sticky").classList.add("stickyadd");
+    } else {
+        document.querySelector(".sticky").classList.remove("stickyadd");
+    }
+}
+
+window.addEventListener('scroll', function () {
+    scrollActive();
+    handleStickyHeader();
+});
+
+// Trigger scrollActive and handleStickyHeader once on page load to set the initial state
+scrollActive();
+handleStickyHeader();
 
 });
 
